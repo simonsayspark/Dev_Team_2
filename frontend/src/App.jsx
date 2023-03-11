80fa7dddff268a05d27736a3fd3eb02148c5c137
Mikecamdo <mcdoherty@smu.edu>
2023-03-11 13:09:26 -0600

    Added LandingPage.jsx
    Added a LandingPage.jsx and changed it to the default page for the website (before it was LoginPage)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { createContext, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LandingPage } from './LandingPage';
 import { LoginPage } from './LoginPage';
 import { HomePage } from './HomePage';
 
@@ -8,8 +9,9 @@ export const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={ <LoginPage/>} />
-                <Route path='/home' element={ <HomePage/> } />
+                <Route path='/' element={ <LandingPage/> }/>
+                <Route path='/login' element={ <LoginPage/>}/>
+                <Route path='/home' element={ <HomePage/> }/>
             </Routes>
         </Router>
     )
