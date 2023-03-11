06cbee34e94f9b8413d9a2012b5860f49a816530
Mikecamdo <mcdoherty@smu.edu>
2023-03-11 15:32:57 -0600

    React-Bootstrap and NavBar on LandingPage
    Added a fixed nav bar to the landing page using react-bootstrap

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,6 +1,42 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom';
 
 export const LandingPage = () => {
     return <>
+        <Navbar fixed='top' bg='light'>
+            <Container fluid className='m-0'>
+                <Navbar.Brand >Website Name</Navbar.Brand>
+                <Navbar.Text>
+                    <NavLink to={'/login'} className='nav-link'>
+                        Log in
+                    </NavLink>
+                </Navbar.Text>
+            </Container>
+        </Navbar>
+        
+        <Button className='mt-5'>I am a button</Button>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
+        <p>Landing Page</p>
         <p>Landing Page</p>
     </>
 }
