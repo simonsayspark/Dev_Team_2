ab66e3b0fa63d456bb85438b80f6e9a253bdddd7
Mikecamdo <mcdoherty@smu.edu>
2023-03-11 23:40:35 -0600

    Frontend and backend connection
    Given you have a mysql server with the given parameters, users can now be created and stored in the backend. Also fixed some errors in the sql setup file.

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE IF NOT EXISTS db;
+CREATE DATABASE IF NOT EXISTS DBUI;
 
-USE db;
+USE DBUI;
+
+create table ceo (
+    ceo_id int auto_increment primary key,
+    cname VARCHAR (50)
+);
 
 create table companies (
-    company_id int AUTOINCREMENT primary key,
+    company_id int auto_increment primary key,
     company_name VARCHAR (50) UNIQUE NOT NULL,
     ceo_id int,
-    foreign key(ceo_id) references ceo
+    foreign key(ceo_id) references ceo(ceo_id)
 );
 
 create table employees (
-    employee_id int AUTOINCREMENT primary key,
+    employee_id int auto_increment primary key,
     ename VARCHAR (50),
     epassword VARCHAR (50),
     role VARCHAR (50),
     company_id int,
-    foreign key(company_id) references companies
-);
-
-create table ceo (
-    ceo_id int AUTOINCREMENT primary key,
-    cname VARCHAR (50)
+    foreign key(company_id) references companies(company_id)
 );
 
 --create table claims (
--- backend/server.js
+++ backend/server.js
@@ -14,69 +14,22 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'CoolPasswordThanks',
+  password: 'NotSoCoolPassword',
   database: 'DBUI'
 })
 
 connection.connect()
 
 // API routes
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.put('/parse', (req, res) => {
-    console.log(req.body)
-    
-    try {
-        const { first, last, age, admin } = req.body
-        const name = `${first} ${last}`
-        const isAdmin = admin ? "is an admin" : "is not an admin"
-
-        res.status(200)
-        res.send(`${name} is ${age} years old and ${isAdmin}`)
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-app.get('/db', (req, res) => {
-    connection.query('SHOW TABLES', (err, rows, fields) =>{
-        if (err) throw err
-
-        console.log(rows)
-        res.status(200)
-        res.send(rows)
-    })
-})
-
-app.post('/user', (req, res) => {
-    const { first, last, age, admin } = req.body
-    const query = `INSERT INTO users (first_name, last_name, age, admin) VALUES ('${first}', '${last}', ${age}, ${admin})`
+app.post('/employees', async (req, res) => {
+    const { name, password, role, company_id } = req.body;
+    const query = `INSERT INTO employees (ename, epassword, role, company_id) VALUES ('${name}', '${password}', '${role}', ${company_id})`;
     connection.query(query, (err, rows, fields) => {
         if (err) throw err
 
         console.log(rows)
         res.status(200)
-        res.send("Successfully added user!")
-    })
-})
-
-app.get('/users', (req, res) => {
-    connection.query(`SELECT * FROM users`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send(rows)
-    })
-})
-
-app.put('/users/clear', (req, res) => {
-    connection.query(`DELETE FROM users`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send("Successfully cleared users!")
+        res.send('Successfully added user!')
     })
 })
 
