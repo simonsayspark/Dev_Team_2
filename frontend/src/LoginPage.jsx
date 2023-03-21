2762136d7cb35a0115665ac71afd6c13b6c3f1bd
jadennathaniel <jaden.prawiro@gmail.com>
2023-03-21 15:00:37 -0500

    added navbar links and styled login login page

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,34 +1,53 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { NavLink } from 'react-router-dom';
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { NavLink } from "react-router-dom";
+import React, {useState} from "react";
 
 export const LoginPage = () => {
-    return <>
-        <Form>
-            <Form.Group className='mb-3' controlId='name'>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='name' placeholder='Enter name'/>
-            </Form.Group>
-            
-            <Form.Group className='mb-3' controlId='password'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Enter password'/>
-            </Form.Group>
+  const [validated, setValidated] = useState(false);
 
-            <Button>
-                Sign in
-            </Button>
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-            <Container fluid>
-                <Row className='text-center mt-5'>
-                    <Col>Don't have an account? <NavLink to={'/signup'} className='nav-link'>Sign up</NavLink></Col>
-                </Row>
-            </Container>
+    setValidated(true);
+  };
+  return (
+    <>
+      <div className="wrapper d-flex justify-content-center align-items-center">
+        <Form noValidate validated={validated} onSubmit={handleSubmit} className="rounded p-4 p-sm-3">
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control required type="name" placeholder="Enter name" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control required type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+
+          <Container fluid>
+            <Row className="text-center mt-5">
+              <Col>
+                Don't have an account?{" "}
+                <NavLink to={"/signup"} className="nav-link">
+                  Sign up
+                </NavLink>
+              </Col>
+            </Row>
+          </Container>
         </Form>
+      </div>
     </>
-
-}
+  );
+};
