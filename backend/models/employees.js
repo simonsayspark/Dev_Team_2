bef43874a46a3a0831c4fba1eeb35f644370cb2f
Mikecamdo <mcdoherty@smu.edu>
2023-03-24 23:46:16 -0500

    Password hashing
    Added password hashing when creating an employee

--- backend/models/employees.js
+++ backend/models/employees.js
@@ -1,9 +1,12 @@
 const knex = require('../database/knex');
 const EMPLOYEES_TABLE = 'employees';
+const bcrypt = require('bcrypt');
 
 const createEmployee = async (ename, epassword, role, company_id) => {
-    //FIXME need to add hashing password here
-    const query = knex(EMPLOYEES_TABLE).insert({ename, epassword, role, company_id});
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(epassword, salt);
+
+    const query = knex(EMPLOYEES_TABLE).insert({ename, hashedPassword, role, company_id});
     const results = await query;
     return results;
 }
