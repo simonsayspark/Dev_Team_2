e646997d7042676e85d0a4ad777062fb1e5b2fe1
simonsayspark <78330778+simonsayspark@users.noreply.github.com>
2023-03-31 16:20:30 -0500

    Added things to landing page Part 1
    - carousel
    - about us
    - contact info

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -5,6 +5,8 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import { Link, NavLink } from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel';
+
 
 export const LandingPage = () => {
   return (
@@ -36,6 +38,7 @@ export const LandingPage = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
       <Container fluid>
         <Row className="text-center mt-5 ">
           <Col className="display-2">
@@ -45,6 +48,52 @@ export const LandingPage = () => {
         </Row>
         <Row className="">About us</Row>
       </Container>
+
+      <Container>
+
+
+
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/Ceo-img.png"
+              alt="Shrek slide"
+            />
+            <Carousel.Caption>
+              <h3>CEO</h3>
+              <p>Create and manage your company with ease.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/tyler-franta-iusJ25iYu1c-unsplash.jpg"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Financial Manager</h3>
+              <p>Approve and deny transactions.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/expense-reimbursements.png"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Employee</h3>
+              <p>
+                Submit transaction forms.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+      </Container>
       {/* 
         Add more content down here, such as an about section, a mission
         statement, etc.
@@ -53,6 +102,34 @@ export const LandingPage = () => {
         https://evrone.com/
         https://nomadictribe.com/
         */}
+
+      <Container>
+        <Row>
+
+          <Col>
+
+
+          </Col>
+        </Row>
+
+
+      </Container>
+
+
+
+      <Container>
+        <Row>
+          <Col>
+            <p> Contaat info</p>
+            <p><strong>Name:</strong>DoughBack</p>
+            <p><strong>Email:</strong>DoughBack@gmail.com</p>
+            <p><strong>Phone:</strong> 123.456.789</p>
+            <p><strong>Address:</strong>6425 Boaz Lane Dallas TX 75205</p>
+
+          </Col>
+        </Row>
+      </Container>
+
     </>
   );
 };
