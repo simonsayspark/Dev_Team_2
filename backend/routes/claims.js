048470ce87a045f2ce547753350edb0360bfb092
Mikecamdo <mcdoherty@smu.edu>
2023-04-10 17:39:01 -0500

    Add claim route
    Added the route for adding a claim/transaction to the database

--- backend/routes/claims.js
+++ backend/routes/claims.js
@@ -3,13 +3,20 @@ const express = require('express');
 router = express.Router();
 router.use(bodyParser.json());
 
+router.post('/', async (req, res, next) => {
+    const { employee_id, company_id, order_date, amount_requested, category, claim_description} = req.body;
+    const addClaim = await req.models.claims.createClaim(employee_id, company_id, order_date, amount_requested, category, claim_description);
+    res.status(201).json(addClaim);    
+    next();
+})
+
 router.get('/', async (req, res, next) => {
     if (req.query.id) {
-        const claimByNumber = await req.models.ceo.getClaimByNumber(req.query.id);
+        const claimByNumber = await req.models.claims.getClaimByNumber(req.query.id);
         res.json(claimByNumber);
         next();
     } else {
-        const allClaims = await req.models.ceo.getAllClaims();
+        const allClaims = await req.models.claims.getAllClaims();
         res.json(allClaims);
         next();
     }
