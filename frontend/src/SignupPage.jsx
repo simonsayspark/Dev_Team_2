d93cf9b4a151fc3495da094bf847918b1da636f7
jadennathaniel <jaden.prawiro@gmail.com>
2023-04-12 13:02:26 -0500

    fixed transaction form

--- frontend/src/SignupPage.jsx
+++ frontend/src/SignupPage.jsx
@@ -11,6 +11,7 @@ import { getCompanies } from "./api/companiesApi";
 import { useNavigate } from "react-router-dom";
 import Dropdown from 'react-bootstrap/Dropdown'
 import Navbar from 'react-bootstrap/Navbar';
+import Alert from "react-bootstrap/Alert";
 import DropdownButton from 'react-bootstrap/DropdownButton';
 //CEO, Finacial Manager, Employee
 const employeeValues = {
@@ -18,7 +19,7 @@ const employeeValues = {
   email: "",
   password: "",
   role: "",
-  company_id: -1
+  company_id: 0
 };
 export const SignupPage = () => {
   const [values, setValues] = useState(employeeValues);
@@ -45,9 +46,6 @@ export const SignupPage = () => {
   const navigate = useNavigate();
 
   const handleRoleSelect = (e) => {
-    if (disableButton) {
-      setDisableButton(false) //FIXME maybe disable button until all fields are filled??
-    }
     setRoleValue(e)
     setValues({ ...values, role: e })
   }
@@ -61,10 +59,7 @@ export const SignupPage = () => {
         id = company.company_id;
       }
     });
-    console.log('ID:')
-    console.log(id)
     setValues({ ...values, company_id: id })
-    console.log(values)
   }
 
   const createAccount = () => {
@@ -79,7 +74,13 @@ export const SignupPage = () => {
       addCeo(ceoValues).then(navigate("/"));
     } else { //Employee or Financial Manager
       console.log('Adding Employee/Financial Manager');
-      addEmployee(values).then(navigate("/"));
+      addEmployee(values).then((e) => {
+        if (!e['message']) {
+          navigate("/");
+        } else {
+          setError(e['message']);
+        }
+      });
     }
   };
 
@@ -87,105 +88,114 @@ export const SignupPage = () => {
     <>
       <Navbar sticky="top" bg="light" expand="lg">
         <Container fluid className="m-0">
-          <Navbar.Brand><NavLink to={"/"} className="nav-link">Website Name</NavLink></Navbar.Brand>
+          <Navbar.Brand><NavLink to={"/"} className="nav-link">DoughBack</NavLink></Navbar.Brand>
         </Container>
       </Navbar>
 
-      <div className="wrapper d-flex justify-content-center align-items-center">
-        <Form>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="name"
-              placeholder="Enter name"
-              value={values.name}
-              onChange={(delta) => {
-                setValues({ ...values, name: delta.target.value });
+      <Container className="pt-5">
+        <div className="bg-light p-3 mx-auto p-md-5 pb-md-3 col-xl-6 mb-4">
+          {error !== "" && 
+            <Alert key={'danger'} variant={'danger'}>
+              {error}
+            </Alert>
+          }
+          
+          <Form>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter name"
+                value={values.name}
+                onChange={(delta) => {
+                  setValues({ ...values, name: delta.target.value });
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={values.email}
+                onChange={(delta) => {
+                  setValues({ ...values, email: delta.target.value });
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={values.password}
+                onChange={(delta) =>
+                  setValues({ ...values, password: delta.target.value })
+                }
+              />
+            </Form.Group>
+
+            <Dropdown
+              className="mt-2"
+              onSelect={handleRoleSelect}>
+
+              <Dropdown.Toggle className="col-12" variant="info" id="dropdown-menu">
+                {roleValue}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="col-12">
+                <Dropdown.Item eventKey='CEO'>CEO</Dropdown.Item>
+                <Dropdown.Item eventKey='Financial Manager'>Financial Manager</Dropdown.Item>
+                <Dropdown.Item eventKey='Employee'>Employee</Dropdown.Item>
+              </Dropdown.Menu>
+
+            </Dropdown>
+
+            <Dropdown
+              className="mt-3"
+              onSelect={handleCompanySelect}>
+
+              <Dropdown.Toggle className="col-12" variant="info" id="dropdown-menu">
+                {companyValue}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="col-12">
+                { companies !== undefined &&
+                  companies.map((company) => {
+                    return (
+                      <Dropdown.Item eventKey={company.company_name}>
+                        {company.company_name}
+                      </Dropdown.Item>
+                    )}
+                  )
+                }
+              </Dropdown.Menu>
+
+            </Dropdown>
+
+            <Button
+              className="col-12 mt-3"
+              disabled={disableButton}
+              onClick={() => {
+                createAccount();
               }}
-            />
-          </Form.Group>
+            >
+              Sign up
+            </Button>
 
-          <Form.Group className="mb-3" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={values.email}
-              onChange={(delta) => {
-                setValues({ ...values, email: delta.target.value });
-              }}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              value={values.password}
-              onChange={(delta) =>
-                setValues({ ...values, password: delta.target.value })
-              }
-            />
-          </Form.Group>
-
-          <Dropdown
-            onSelect={handleRoleSelect}>
-
-            <Dropdown.Toggle variant="success" id="dropdown-menu">
-              {roleValue}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item eventKey='CEO'>CEO</Dropdown.Item>
-              <Dropdown.Item eventKey='Financial Manager'>Financial Manager</Dropdown.Item>
-              <Dropdown.Item eventKey='Employee'>Employee</Dropdown.Item>
-            </Dropdown.Menu>
-
-          </Dropdown>
-
-          <Dropdown
-            onSelect={handleCompanySelect}>
-
-            <Dropdown.Toggle variant="success" id="dropdown-menu">
-              {companyValue}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              { companies !== undefined &&
-                companies.map((company) => {
-                  return (
-                    <Dropdown.Item eventKey={company.company_name}>
-                      {company.company_name}
-                    </Dropdown.Item>
-                  )}
-                )
-              }
-            </Dropdown.Menu>
-
-          </Dropdown>
-
-
-          <Button
-            type="submit"
-            disabled={disableButton}
-            onClick={() => {
-              createAccount();
-            }}
-          >
-            Sign up
-          </Button>
-
-          <Container fluid>
-            <Row className="text-center mt-5">
-              <Col>
-                Already have an account?{" "}
-                <NavLink to={"/login"} className="nav-link">
-                  Sign in
-                </NavLink>
-              </Col>
-            </Row>
-          </Container>
-        </Form>
-      </div>
+            <Container fluid>
+              <Row className="text-center mt-4">
+                <Col>
+                  Already have an account?
+                  <NavLink to={"/login"} className="nav-link">
+                    Sign in
+                  </NavLink>
+                </Col>
+              </Row>
+            </Container>
+          </Form>
+        </div>
+      </Container>
     </>
   );
 };
--- frontend/src/components/transaction/transactionList.jsx
+++ frontend/src/components/transaction/transactionList.jsx
@@ -1,211 +1,182 @@
-
 import { useContext } from "react";
 import { UserContext } from "../../App";
 import { TransactionForm } from "./transactionForm";
 import { useState, useEffect } from "react";
-import { getTransactionById, getTransactionByStatus } from "../../api/transactionApi";
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import ListGroup from 'react-bootstrap/ListGroup';
+import {
+  getTransactionById,
+  getTransactionByStatus,
+} from "../../api/transactionApi";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
-import Badge from 'react-bootstrap/Badge';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Badge from "react-bootstrap/Badge";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
-
-//ONlY for pending, allow for edits of the transaction details
+//ONLY for pending, allow for edits of the transaction details
 
 export const TransactionList = () => {
-    const currentUser = useContext(UserContext);
+  const currentUser = useContext(UserContext);
 
-    const [aTransactions, setaTransactions] = useState(undefined);
-    const [dTransactions, setdTransactions] = useState(undefined);
-    const [pTransactions, setpTransactions] = useState(undefined);
-    useEffect(() => {
-        getTransactionByStatus(currentUser.employee_id, "Accepted").then((x) => setaTransactions(x));
-        getTransactionByStatus(currentUser.employee_id, "Denied").then((x) => setdTransactions(x));
-        getTransactionByStatus(currentUser.employee_id, "Pending").then((x) => setpTransactions(x));
-    }, [])
+  const [aTransactions, setaTransactions] = useState(undefined);
+  const [dTransactions, setdTransactions] = useState(undefined);
+  const [pTransactions, setpTransactions] = useState(undefined);
+  useEffect(() => {
+    getTransactionByStatus(currentUser.employee_id, "Accepted").then((x) =>
+      setaTransactions(x)
+    );
+    getTransactionByStatus(currentUser.employee_id, "Denied").then((x) =>
+      setdTransactions(x)
+    );
+    getTransactionByStatus(currentUser.employee_id, "Pending").then((x) =>
+      setpTransactions(x)
+    );
+  }, []);
 
-    //create 3 different api requests 
+  //create 3 different api requests
 
-    { console.log("here") }
+  {
+    console.log("here");
+  }
 
-    return (
-        <>
-            {console.log('D Transactions')}
-            {console.log(dTransactions)}
-            <Tabs
-                defaultActiveKey="profile"
-                id="uncontrolled-tab-example"
-                className="mb-3">
+  return (
+    <>
+      {console.log("D Transactions")}
+      {console.log(dTransactions)}
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="pending" title="Pending">
+          {pTransactions.length !== 0 ? (
+            <ListGroup>
+              {pTransactions.map((transaction, index) => {
+                return (
+                  <ListGroup.Item>
+                    <Container>
+                      <Row>
+                        <Col className="p-0">{transaction.order_date}</Col>
+                        <Col>
+                          <Badge bg="secondary" className="">
+                            {transaction.claim_status}
+                          </Badge>{" "}
+                        </Col>
+                      </Row>
 
+                      <Row>
+                        Amount Requested: ${transaction.amount_requested}
+                      </Row>
 
-                <Tab eventKey="pending" title="Pending">
+                      <Row>
+                        Claim Description:
+                        <br />
+                        {transaction.claim_description}
+                      </Row>
+                    </Container>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          ) : (
+            <p>No available transaction</p>
+          )}
+        </Tab>
 
-                    {pTransactions.length !== 0 ?
-                        <ListGroup>
-                            {
-                                pTransactions.map((transaction, index) => {
-                                    return (
-                                        <ListGroup.Item>
-                                            <Container>
-                                                <Row>
-                                                    <Col className="p-0">{transaction.order_date}</Col>
-                                                    <Col><Badge bg="secondary" className="">{transaction.claim_status}</Badge> </Col>
+        <Tab eventKey="accepted" title="Accepted">
+          {aTransactions.length !== 0 ? (
+            <ListGroup>
+              {aTransactions.map((transaction, index) => {
+                return (
+                  <ListGroup.Item>
+                    <Container>
+                      <Row>
+                        <Col className="p-0">{transaction.order_date}</Col>
+                        <Col>
+                          <Badge bg="secondary" className="">
+                            {transaction.claim_status}
+                          </Badge>{" "}
+                        </Col>
+                      </Row>
 
-                                                </Row>
+                      <Row>
+                        Amount Requested: ${transaction.amount_requested}
+                      </Row>
 
-                                                <Row>
-                                                    Amount Requested:
-                                                    ${transaction.amount_requested}
+                      <Row>
+                        Amount Reimbursed: ${transaction.amount_reimbursed}
+                      </Row>
 
-                                                </Row>
+                      <Row>
+                        Claim Description:
+                        <br />
+                        {transaction.claim_description}
+                      </Row>
 
-                                                <Row>
-                                                    Claim Description:
-                                                    <br />
-                                                    {transaction.claim_description}
+                      <Row>
+                        Ceo Comment:
+                        <br />
+                        {transaction.ceo_comment}
+                      </Row>
+                    </Container>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          ) : (
+            <p>No available transaction</p>
+          )}
+        </Tab>
 
-                                                </Row>
+        <Tab eventKey="denied" title="Denied">
+          {dTransactions.length !== 0 ? (
+            <ListGroup>
+              {dTransactions.map((transaction, index) => {
+                return (
+                  <ListGroup.Item>
+                    <Container>
+                      <Row>
+                        <Col className="p-0">{transaction.order_date}</Col>
+                        <Col>
+                          <Badge bg="secondary" className="">
+                            {transaction.claim_status}
+                          </Badge>{" "}
+                        </Col>
+                      </Row>
 
+                      <Row>
+                        Amount Requested: ${transaction.amount_requested}
+                      </Row>
 
-                                            </Container>
-                                        </ListGroup.Item>
+                      <Row>
+                        Amount Reimbursed: ${transaction.amount_reimbursed}
+                      </Row>
 
-                                    );
-                                }
-                                )}
-                        </ListGroup> : (
-                            <p>No available transaction</p>
-                        )
+                      <Row>
+                        Claim Description:
+                        <br />
+                        {transaction.claim_description}
+                      </Row>
 
+                      <Row>
+                        Ceo Comment:
+                        <br />
+                        {transaction.ceo_comment}
+                      </Row>
+                    </Container>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          ) : (
+            <p>No available transaction</p>
+          )}
+        </Tab>
+      </Tabs>
 
-                    }
-
-                </Tab>
-
-                <Tab eventKey="accepted" title="Accepted">
-                    {aTransactions.length !== 0 ?
-                        <ListGroup>
-                            {
-                                aTransactions.map((transaction, index) => {
-                                    return (
-                                        <ListGroup.Item>
-                                            <Container>
-                                                <Row>
-                                                    <Col className="p-0">{transaction.order_date}</Col>
-                                                    <Col><Badge bg="secondary" className="">{transaction.claim_status}</Badge> </Col>
-
-                                                </Row>
-
-                                                <Row>
-                                                    Amount Requested:
-                                                    ${transaction.amount_requested}
-
-                                                </Row>
-
-                                                <Row>
-                                                    Amount Reimbursed:
-                                                    ${transaction.amount_reimbursed}
-
-                                                </Row>
-
-                                                <Row>
-                                                    Claim Description:
-                                                    <br />
-                                                    {transaction.claim_description}
-
-                                                </Row>
-
-                                                <Row>
-                                                    Ceo Comment:
-                                                    <br />
-                                                    {transaction.ceo_comment}
-
-                                                </Row>
-
-
-                                            </Container>
-                                        </ListGroup.Item>
-
-                                    );
-                                }
-                                )}
-                        </ListGroup> : (
-                            <p>No available transaction</p>
-                        )
-
-
-                    }
-
-
-                </Tab>
-
-                <Tab eventKey="denied" title="Denied">
-                    {dTransactions.length !== 0 ?
-                        <ListGroup>
-                            {
-                                dTransactions.map((transaction, index) => {
-                                    return (
-                                        <ListGroup.Item>
-                                            <Container>
-                                                <Row>
-                                                    <Col className="p-0">{transaction.order_date}</Col>
-                                                    <Col><Badge bg="secondary" className="">{transaction.claim_status}</Badge> </Col>
-
-                                                </Row>
-
-                                                <Row>
-                                                    Amount Requested:
-                                                    ${transaction.amount_requested}
-
-                                                </Row>
-
-
-                                                <Row>
-                                                    Amount Reimbursed:
-                                                    ${transaction.amount_reimbursed}
-
-                                                </Row>
-
-                                                <Row>
-                                                    Claim Description:
-                                                    <br />
-                                                    {transaction.claim_description}
-
-                                                </Row>
-
-                                                <Row>
-                                                    Ceo Comment:
-                                                    <br />
-                                                    {transaction.ceo_comment}
-
-                                                </Row>
-
-
-                                            </Container>
-                                        </ListGroup.Item>
-
-                                    );
-                                }
-                                )}
-                        </ListGroup> : (
-                            <p>No available transaction</p>
-                        )
-
-
-                    }
-
-
-                </Tab>
-
-
-
-            </Tabs>
-
-            {/* {transactions ? transactions.map((transaction, index) => {
+      {/* {transactions ? transactions.map((transaction, index) => {
                 return (
                     <ListGroup key={index}>
 
@@ -245,7 +216,6 @@ export const TransactionList = () => {
 
 
             } */}
-        </>);
-
-}
-
+    </>
+  );
+};
