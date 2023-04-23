9d7ef505afb9aae24fa2bfcf9bd18bdad162e8c2
Mikecamdo <mcdoherty@smu.edu>
2023-04-23 02:52:34 -0500

    Homepage styling
    Adding homepage styling for Financial Managers and Ceos

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -19,23 +19,13 @@ export const HomePage = ({ setCurrentUser }) => {
   function logout() {
     setCurrentUser(undefined);
   }
-  function handleClick(){
-    navigate("/addTransaction")
 
-  }
-
-  function handleClick2(){
-    navigate("/viewTransactions")
-
-  }
-
-  if (currentUser.ceo_id) {
-    //CEO homepage
+  if (currentUser.ceo_id) { //CEO homepage
     return (
       <>
-        <Navbar sticky="top" className="color-nav" expand="lg">
+        <Navbar sticky="top" className="color-nav" expand="md" collapseOnSelect>
           <Container fluid className="m-0">
-            <Navbar.Brand>
+            <Navbar.Brand className="theBrand">
               <NavLink to={"/home"} className="nav-link">
                 <img
                   width="300px"
@@ -45,76 +35,111 @@ export const HomePage = ({ setCurrentUser }) => {
                 />
               </NavLink>
             </Navbar.Brand>
-            <Navbar.Text>
-              <Link
-                to={"/"}
-                className="nav-link text-light fs-4 mx-5 text-nowrap"
-                onClick={() => {
-                  setCurrentUser(undefined);
-                }}
-              >
-                Log out
-              </Link>
-            </Navbar.Text>
+            <Navbar.Toggle />
+            <NavbarCollapse>
+              <Nav className="justify-content-end" style={{ width: "100%" }}>
+                <NavLink
+                  to={"/"}
+                  className="fs-4 nav-link text-light mx-5"
+                  onClick={() => {
+                    setCurrentUser(undefined);
+                  }}
+                >
+                  Log out
+                </NavLink>
+              </Nav>
+            </NavbarCollapse>
           </Container>
         </Navbar>
+
         <Container className="mt-3">
-          <h1 className="display-4">Hello {currentUser.cname}!</h1>
+          <Row className="display-1 text-center">
+            <p>{currentUser.cname}'s Dashboard</p>
+          </Row>
+
           <NavLink to={"/viewTransactions"} className="d-flex mb-3">
-            <button className="btn btn-primary">
+            <Button className="submitButton">
               {" "}
               View Company Transactions
-            </button>
+            </Button>
           </NavLink>
           <NavLink to={"/viewTransactions"} className="d-flex mb-3">
-            <button className="btn btn-primary">
+            <Button className="submitButton">
               {" "}
               Add and remove employees
-            </button>
+            </Button>
           </NavLink>
         </Container>
-      <Container className="mt-3">
-        <h1 className="display-4">Hello {currentUser.cname}!</h1>
-        <NavLink to={"/viewTransactions"} className="d-flex mb-3">
-          <button className="btn btn-primary btn-lg">
-            View Company Transactions
-          </button>
-        </NavLink>
-        <NavLink to={"/viewTransactions"} className="d-flex mb-3">
-          <button className="btn btn-primary btn-lg">
-            Add and remove employees
-          </button>
-        </NavLink>
-      </Container>
-    </>
+        <Container className="mt-3">
+          <NavLink to={"/viewTransactions"} className="d-flex mb-3">
+            <Button className="submitButton btn-lg">
+              View Company Transactions
+            </Button>
+          </NavLink>
+          <NavLink to={"/viewTransactions"} className="d-flex mb-3">
+            <Button className="submitButton btn-lg">
+              Add and remove employees
+            </Button>
+          </NavLink>
+        </Container>
+      </>
     );
   } else if (currentUser.role === "Financial Manager") { //Financial Manager homepage
     return (
       <>
-        <Navbar sticky="top" className="color-nav" expand="lg">
+        <Navbar sticky="top" className="color-nav" expand="md" collapseOnSelect>
           <Container fluid className="m-0">
-            <img className="logo" src="/logo_text.png" alt="logo" />
-            <Navbar.Brand>
-              <NavLink to={"/home"} className="nav-link"></NavLink>
+            <Navbar.Brand className="theBrand">
+              <NavLink to={"/home"} className="nav-link">
+                <img
+                  width="300px"
+                  height="auto"
+                  src="/logo_text.png"
+                  alt="logo"
+                />
+              </NavLink>
             </Navbar.Brand>
-            <Navbar.Text>
-              <Link
-                to={"/"}
-                className="nav-link text-light fs-4 mx-5 text-nowrap"
-                onClick={() => {
-                  setCurrentUser(undefined);
-                }}
-              >
-                Log out
-              </Link>
-            </Navbar.Text>
+            <Navbar.Toggle />
+            <NavbarCollapse>
+              <Nav className="justify-content-end" style={{ width: "100%" }}>
+                <NavLink
+                  to={"/"}
+                  className="fs-4 nav-link text-light mx-5"
+                  onClick={() => {
+                    setCurrentUser(undefined);
+                  }}
+                >
+                  Log out
+                </NavLink>
+              </Nav>
+            </NavbarCollapse>
           </Container>
         </Navbar>
+
         <Container className="mt-3">
-          <h1 className="display-4">Hello {currentUser.ename}!</h1>
-          <NavLink to={"/viewTransactions"} className="d-flex mb-3">
-            <button className="btn btn-primary"> View Transactions</button>
-          </NavLink>
+          <Row className="display-1 text-center">
+            <p>{currentUser.ename}'s Dashboard</p>
+          </Row>
+
+          <Row className="w-100">
+            <Card className="bg-light text-center homePageCards">
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>
+                  Approve and Deny Transactions
+                </Card.Title>
+                <Card.Text>
+                  Here, you can view all of the pending reimbursement requests
+                  submitted by employees in your company.
+                </Card.Text>
+                <Button className="mt-auto submitButton"
+                  onClick={() => {
+                    navigate("/viewTransactions")
+                  }}>
+                  View Reimbursement Requests
+                </Button>
+              </Card.Body>
+            </Card>
+          </Row>
         </Container>
         ;
       </>
@@ -134,59 +159,58 @@ export const HomePage = ({ setCurrentUser }) => {
                 />
               </NavLink>
             </Navbar.Brand>
-            <Navbar.Toggle/>
+            <Navbar.Toggle />
             <NavbarCollapse>
               <Nav className="justify-content-end" style={{ width: "100%" }}>
-                  <NavLink
-                        to={"/"}
-                        className="fs-4 nav-link text-light mx-5"
-                        onClick={() => {
-                          setCurrentUser(undefined);
-                        }}
-                      >
-                        Log out
-                  </NavLink>
+                <NavLink
+                  to={"/"}
+                  className="fs-4 nav-link text-light mx-5"
+                  onClick={() => {
+                    setCurrentUser(undefined);
+                  }}
+                >
+                  Log out
+                </NavLink>
               </Nav>
             </NavbarCollapse>
           </Container>
         </Navbar>
 
         <Container className="mt-3">
-        
-            <Row className="display-1 text-center">
-              <p>{currentUser.ename}'s Dashboard</p>
-            </Row>
-            
-            <Row className="w-100 homePagePadding">
-              <Col className="bg-light text-center me-1 homePageCards">
-                <Card>
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>
-                      Submit Transactions
-                    </Card.Title>
-                    <Card.Text>
-                      Here, you can submit a reimbursement request for a business
-                      expense that you made on behalf of your company.
-                    </Card.Text>
-                    <Button className="mt-auto submitButton" onClick={()=>handleClick()}>Submit a transaction</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col className="bg-light text-center ms-1 homePageCards">
-                <Card>
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>
-                      View Transactions
-                    </Card.Title>
-                    <Card.Text>
-                      Here, you can view all of your previous reimbursement requests,
-                      as well as edit, delete, or appeal your submitted transactions.
-                    </Card.Text>
-                    <Button className="mt-auto submitButton" onClick={()=>handleClick2()}>View your transactions</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
+          <Row className="display-1 text-center">
+            <p>{currentUser.ename}'s Dashboard</p>
+          </Row>
+
+          <Row className="w-100">
+            <Col className="bg-light text-center me-1 homePageCards">
+              <Card>
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>
+                    Submit Transactions
+                  </Card.Title>
+                  <Card.Text>
+                    Here, you can submit a reimbursement request for a business
+                    expense that you made on behalf of your company.
+                  </Card.Text>
+                  <Button className="mt-auto submitButton" onClick={() => navigate("/addTransaction")}>Submit a transaction</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="bg-light text-center ms-1 homePageCards">
+              <Card>
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title>
+                    View Transactions
+                  </Card.Title>
+                  <Card.Text>
+                    Here, you can view all of your previous reimbursement requests,
+                    as well as edit, delete, or appeal your submitted transactions.
+                  </Card.Text>
+                  <Button className="mt-auto submitButton" onClick={() => navigate("/viewTransactions")}>View your transactions</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Container>
       </>
     );
