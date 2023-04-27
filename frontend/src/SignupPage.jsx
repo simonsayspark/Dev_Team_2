e7594cbe6b5ba008a5240e15742c6badd5270704
Mikecamdo <mcdoherty@smu.edu>
2023-04-27 18:49:54 -0500

    Got rid of gray background
    Got rid of gray background color on the login and signup page (since the rest of the website just uses a white background

--- frontend/src/SignupPage.jsx
+++ frontend/src/SignupPage.jsx
@@ -213,228 +213,226 @@ export const SignupPage = ({ setCurrentUser }) => {
 
   return (
     <>
-      <div className="light-bg">
-        <Navbar sticky="top" className="color-nav" expand="lg">
-          <Container fluid className="m-0">
-            <Navbar.Brand>
-              <NavLink to={"/"} className="nav-link">
-                <img
-                  width="300px"
-                  height="auto"
-                  src="/logo_text.png"
-                  alt="logo"
-                />
-              </NavLink>
-            </Navbar.Brand>
-          </Container>
-        </Navbar>
+      <Navbar sticky="top" className="color-nav" expand="lg">
+        <Container fluid className="m-0">
+          <Navbar.Brand>
+            <NavLink to={"/"} className="nav-link">
+              <img
+                width="300px"
+                height="auto"
+                src="/logo_text.png"
+                alt="logo"
+              />
+            </NavLink>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
 
-        <div className="pt-4 mx-0 px-0">
-          <div className="bg-light rounded p-3 mx-auto p-md-5 pb-md-3 col-lg-6 col-sm-8">
-            {error !== "" && (
-              <Alert key={"danger"} variant={"danger"}>
-                {error}
-              </Alert>
-            )}
+      <div className="pt-4 mx-0 px-0">
+        <div className="bg-light rounded p-3 mx-auto p-md-5 pb-md-3 col-lg-6 col-sm-8">
+          {error !== "" && (
+            <Alert key={"danger"} variant={"danger"}>
+              {error}
+            </Alert>
+          )}
 
-            <Form>
-              <Form.Group className="" controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="name"
-                  placeholder="Enter name"
-                  value={values.name}
-                  onChange={(delta) => {
-                    setValues({ ...values, name: delta.target.value });
-                  }}
-                />
-              </Form.Group>
+          <Form>
+            <Form.Group className="" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter name"
+                value={values.name}
+                onChange={(delta) => {
+                  setValues({ ...values, name: delta.target.value });
+                }}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-3" controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  value={values.email}
-                  onChange={(delta) => {
-                    setValues({ ...values, email: delta.target.value });
-                  }}
-                />
-              </Form.Group>
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={values.email}
+                onChange={(delta) => {
+                  setValues({ ...values, email: delta.target.value });
+                }}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter password"
-                  value={values.password}
-                  onChange={(delta) =>
-                    setValues({ ...values, password: delta.target.value })
-                  }
-                />
-              </Form.Group>
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={values.password}
+                onChange={(delta) =>
+                  setValues({ ...values, password: delta.target.value })
+                }
+              />
+            </Form.Group>
 
-              <Form.Group controlId="confirmPassword">
-                <Form.Label>Confirm password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Re-enter password"
-                  value={values.confirmPassword}
-                  onChange={(delta) =>
-                    setValues({ ...values, confirmPassword: delta.target.value })
-                  }
-                />
-                
-                {values.password && !passwordValid && (
-                  <Container className=" text-muted">
-                    <Row className="text-center">
-                      <span className="text-center">
-                        Password Requirements:
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Re-enter password"
+                value={values.confirmPassword}
+                onChange={(delta) =>
+                  setValues({ ...values, confirmPassword: delta.target.value })
+                }
+              />
+
+              {values.password && !passwordValid && (
+                <Container className=" text-muted">
+                  <Row className="text-center">
+                    <span className="text-center">
+                      Password Requirements:
+                    </span>
+                    {checkLength === true ? (
+                      <span className="text-center text-success">
+                        &#x2713; At least 8 characters
                       </span>
-                      {checkLength === true ? (
+                    ) : (
+                      <span className="text-center text-danger">
+                        &#x2715; At least 8 characters
+                      </span>
+                    )}
+                  </Row>
+                  <Row>
+                    {checkCapital === true ? (
+                      <span className="text-center text-success">
+                        &#x2713; Have a uppercase letter
+                      </span>
+                    ) : (
+                      <span className="text-center text-danger">
+                        &#x2715; Have a uppercase letter
+                      </span>
+                    )}
+                  </Row>
+                  <Row>
+                    {checkLowerCase === true ? (
+                      <span className="text-center text-success">
+                        &#x2713; Have a lower letter
+                      </span>
+                    ) : (
+                      <span className="text-center text-danger">
+                        &#x2715; Have a lower letter
+                      </span>
+                    )}
+                  </Row>
+                  <Row>
+                    {checkSymbol === true ? (
+                      <span className="text-center text-success">
+                        &#x2713; Have a symbol
+                      </span>
+                    ) : (
+                      <span className="text-center text-danger">
+                        &#x2715; Have a symbol
+                      </span>
+                    )}
+                  </Row>
+                  <Row>
+                    {checkDigit === true ? (
+                      <span className="text-center text-success">
+                        &#x2713; Have a digit
+                      </span>
+                    ) : (
+                      <span className="text-center text-danger">
+                        &#x2715; Have a digit
+                      </span>
+                    )}
+                  </Row>
+                  <Row>
+                    <Row>
+                      {passwordsMatch ? (
                         <span className="text-center text-success">
-                          &#x2713; At least 8 characters
+                          &#x2713; Passwords must match
                         </span>
                       ) : (
                         <span className="text-center text-danger">
-                          &#x2715; At least 8 characters
+                          &#x2715; Passwords must match
                         </span>
                       )}
                     </Row>
-                    <Row>
-                      {checkCapital === true ? (
-                        <span className="text-center text-success">
-                          &#x2713; Have a uppercase letter
-                        </span>
-                      ) : (
-                        <span className="text-center text-danger">
-                          &#x2715; Have a uppercase letter
-                        </span>
-                      )}
-                    </Row>
-                    <Row>
-                      {checkLowerCase === true ? (
-                        <span className="text-center text-success">
-                          &#x2713; Have a lower letter
-                        </span>
-                      ) : (
-                        <span className="text-center text-danger">
-                          &#x2715; Have a lower letter
-                        </span>
-                      )}
-                    </Row>
-                    <Row>
-                      {checkSymbol === true ? (
-                        <span className="text-center text-success">
-                          &#x2713; Have a symbol
-                        </span>
-                      ) : (
-                        <span className="text-center text-danger">
-                          &#x2715; Have a symbol
-                        </span>
-                      )}
-                    </Row>
-                    <Row>
-                      {checkDigit === true ? (
-                        <span className="text-center text-success">
-                          &#x2713; Have a digit
-                        </span>
-                      ) : (
-                        <span className="text-center text-danger">
-                          &#x2715; Have a digit
-                        </span>
-                      )}
-                    </Row>
-                    <Row>
-                      <Row>
-                        {passwordsMatch ? (
-                          <span className="text-center text-success">
-                            &#x2713; Passwords must match
-                          </span>
-                        ) : (
-                          <span className="text-center text-danger">
-                            &#x2715; Passwords must match
-                          </span>
-                        )}
-                      </Row>
-                    </Row>
-                  </Container>
-                )}
-              </Form.Group>
+                  </Row>
+                </Container>
+              )}
+            </Form.Group>
 
-              <Dropdown className="mt-4 dropdown1" onSelect={handleRoleSelect}>
+            <Dropdown className="mt-4 dropdown1" onSelect={handleRoleSelect}>
+              <Dropdown.Toggle
+                className="col-12 dropdown-bg text-white"
+                variant="info"
+                id="dropdown-menu"
+              >
+                {roleValue}
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="col-12 ">
+                <Dropdown.Item className="dropBg" eventKey="CEO">CEO</Dropdown.Item>
+                <Dropdown.Item eventKey="Financial Manager">
+                  Financial Manager
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="Employee">Employee</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            {values.role && values.role !== "CEO" && (
+              <Dropdown className="mt-3 dropdown2" onSelect={handleCompanySelect}>
                 <Dropdown.Toggle
-                  className="col-12 dropdown-bg text-white"
+                  className="col-12 dropdown-bg-1 text-white"
                   variant="info"
                   id="dropdown-menu"
                 >
-                  {roleValue}
+                  {companyValue}
                 </Dropdown.Toggle>
-                <Dropdown.Menu className="col-12 ">
-                  <Dropdown.Item className="dropBg" eventKey="CEO">CEO</Dropdown.Item>
-                  <Dropdown.Item eventKey="Financial Manager">
-                    Financial Manager
-                  </Dropdown.Item>
-                  <Dropdown.Item eventKey="Employee">Employee</Dropdown.Item>
+                <Dropdown.Menu className="col-12">
+                  {companies !== undefined &&
+                    companies.map((company) => {
+                      return (
+                        <Dropdown.Item eventKey={company.company_name}>
+                          {company.company_name}
+                        </Dropdown.Item>
+                      );
+                    })}
                 </Dropdown.Menu>
               </Dropdown>
+            )}
 
-              {values.role && values.role !== "CEO" && (
-                <Dropdown className="mt-3 dropdown2" onSelect={handleCompanySelect}>
-                  <Dropdown.Toggle
-                    className="col-12 dropdown-bg-1 text-white"
-                    variant="info"
-                    id="dropdown-menu"
-                  >
-                    {companyValue}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu className="col-12">
-                    {companies !== undefined &&
-                      companies.map((company) => {
-                        return (
-                          <Dropdown.Item eventKey={company.company_name}>
-                            {company.company_name}
-                          </Dropdown.Item>
-                        );
-                      })}
-                  </Dropdown.Menu>
-                </Dropdown>
-              )}
+            {values.role == "CEO" && (
+              <Form.Group>
+                <Form.Label className="pt-2">Company Name</Form.Label>
+                <Form.Control
+                  placeholder="Enter company name"
+                  value={ceoCompany}
+                  onChange={(delta) => {
+                    setCeoCompany(delta.target.value);
+                  }}
+                ></Form.Control>
+              </Form.Group>
+            )}
+            <Button
+              className="col-12 mt-3 submitButton"
+              disabled={disableButton}
+              onClick={() => {
+                createAccount();
+              }}
+            >
+              Sign up
+            </Button>
 
-              {values.role == "CEO" && (
-                <Form.Group>
-                  <Form.Label className="pt-2">Company Name</Form.Label>
-                  <Form.Control
-                    placeholder="Enter company name"
-                    value={ceoCompany}
-                    onChange={(delta) => {
-                      setCeoCompany(delta.target.value);
-                    }}
-                  ></Form.Control>
-                </Form.Group>
-              )}
-              <Button
-                className="col-12 mt-3 submitButton"
-                disabled={disableButton}
-                onClick={() => {
-                  createAccount();
-                }}
-              >
-                Sign up
-              </Button>
-
-              <Container fluid>
-                <Row className="text-center mt-4">
-                  <Col>
-                    <p className="text-muted mb-0">Already have an account?</p>
-                    <NavLink to={"/login"} className="sign-up text-decoration-none">
-                      Sign in
-                    </NavLink>
-                  </Col>
-                </Row>
-              </Container>
-            </Form>
-          </div>
+            <Container fluid>
+              <Row className="text-center mt-4">
+                <Col>
+                  <p className="text-muted mb-0">Already have an account?</p>
+                  <NavLink to={"/login"} className="sign-up text-decoration-none">
+                    Sign in
+                  </NavLink>
+                </Col>
+              </Row>
+            </Container>
+          </Form>
         </div>
       </div>
     </>
